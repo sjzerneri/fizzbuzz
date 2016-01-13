5166544a69cb013c3715566108ac7b83d8d1d301
Stevey Z <sjzerneri@gmail.com>
2016-01-12 23:15:58 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    for (i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            $("body").append("<p>Fizzbuzz</p>");
+        } else if (i % 3 === 0) {
+            $("body").append("<p>Fizz</p>");
+        } else if (i % 5 === 0) {
+            $("body").append("<p>Buzz</p>");
+        } else $("body").append('<p>' + i + '</p>');
+    }
+});
