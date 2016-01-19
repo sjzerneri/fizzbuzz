7634b7fd06e2facec9b9866e9e9d9c398ce80288
Stevey Z <sjzerneri@gmail.com>
2016-01-19 17:05:09 -0500

    refactoring code to wrap inside function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,42 @@
-var i = 0;
+ $(document).ready(function () {
 
-$(document).ready(function () {
-    for (i = 1; i <= 100; i++) {
-        if (i % 15 === 0) {
-            $("body").append("<p>Fizzbuzz</p>");
-        } else if (i % 3 === 0) {
-            $("body").append("<p>Fizz</p>");
-        } else if (i % 5 === 0) {
-            $("body").append("<p>Buzz</p>");
-        } else {
-            $("body").append('<p>' + i + '</p>');
-        }
-    }
-});
+     /****************************
+     First declare the functions
+     ****************************/
+
+     //fizzbuz function is creating the game
+     function fizzbuzz(limit) {
+
+         //check the each number and show the results
+         function check(i) {
+
+             var msg = "<p>" + i + "</p>";
+
+
+             if (i % 3 == 0) {
+                 msg = "<p>Fizz</p>";
+             };
+             if (i % 5 == 0) {
+                 msg = "<p>Buzz</p>";
+             };
+             if (i % 15 == 0) {
+                 msg = "<p>Fizzbuzz</p>";
+             };
+             return msg;
+         }
+
+         //run through each number and append the number to the html
+
+         for (var counter = 1; counter <= limit; counter++) {
+             $('body').append(check(counter));
+         }
+     }
+
+
+     //get the upper limit from the user input
+
+     var limit = prompt("Please set the upper limit to play FizzBuzz.");
+
+     fizzbuzz(limit);
+
+ });
